7a5fee12e9f4a6d0f722fbfc42abaf88334ceb5d
Daniel <danielfdoficial@gmail.com>
2025-01-31 20:02:15 -0300

    build: config postgreSQL as project database

--- comandos-usados-no-terminal.js
+++ comandos-usados-no-terminal.js
@@ -2,4 +2,11 @@
 
 // rails - v
 
-// rails new notebook-api --api (para criar uma API rails)
+// rails new notebook-api -T --database=postgresql --api (para criar uma API rails com postgresql)
+
+// rails g scaffold Contact name:string email:string birthdate:date
+
+// rails db:migrate (antes, importante configurar o database.yml para conectar ao banco de dados)
+
+// http://localhost:3000/rails/info/routes (no navegador, para acessar as rotas)
+
