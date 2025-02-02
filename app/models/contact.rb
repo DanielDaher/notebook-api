ae65967b49fdfbe00705e43f2c8a4245a0251df5
Daniel <danielfdoficial@gmail.com>
2025-02-02 18:09:31 -0300

    feat: setting a model method on all controllers requisitions

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,9 @@ class Contact < ApplicationRecord
   def author
     'Daniel' # method para retornar o campo author, usado em contacts_controller.rb
   end
+
+  def as_json(options={})
+    super(methods: :author, root: true) 
+    ## essa função está fazendo duas coisas: adicionando o método author em todas as reqs do contacts_controller.rb, e adicionando a chave root (que no caso será contact);
+  end
 end
